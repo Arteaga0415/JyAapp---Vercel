cf6778df159b0f9db372347cf1c2adb892e88bf5
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-07-24 00:56:16 -0500

    feat: changes applied trying

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
-# main.py
+# api/main.py
 import logging
-import shutil
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+import shutil
 from tempfile import NamedTemporaryFile
 from produccion_ramos import ProduccionRamos
 
@@ -13,9 +13,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 # Set up CORS
-origins = [
-    "*",  # Allows all origins, change this to restrict access to specific origins
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,22 +26,18 @@ app.add_middleware(
 @app.post("/process/")
 async def process_file(file: UploadFile = File(...)):
     try:
-        with NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
+        tmp_file_path = f"/tmp/{file.filename}"
+        with open(tmp_file_path, 'wb') as tmp:
             shutil.copyfileobj(file.file, tmp)
-            tmp_file_path = tmp.name
 
         produccion_ramos = ProduccionRamos(tmp_file_path)
         produccion_ramos.process_data()
         
         output = produccion_ramos.save_to_bytes()
         
-        return StreamingResponse(
-           output,
-           media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-           headers={"Content-Disposition": "attachment; filename=processed_data.xlsx"}
-           )
+        return StreamingResponse(output, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', headers={"Content-Disposition": "attachment; filename=processed_data.xlsx"})
     except Exception as e:
-        logging.error(f"Error processing file: {e}")
+        logger.error(f"Error processing file: {e}")
         return {"error": str(e)}
 
-# To run the app, use the command: uvicorn main:app --reload
+# To run the app locally, use the command: uvicorn api.main:app --reload
--- produccion_ramos.py
+++ produccion_ramos.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sys  
 from io import BytesIO
 
 class ProduccionRamos:
@@ -7,23 +6,23 @@ class ProduccionRamos:
         self.file_path = file_path
         self.cleaned_df = None
         self.grouped_df = None
-    
+
     def load_and_clean_data(self):
         df = pd.read_excel(self.file_path)
         self.cleaned_df = df.dropna(subset=['Aseguradora'])
-    
+
     def add_insurance_columns(self):
         insurance_companies = [
             'SEGUROS SURAMERICANA S.A', 'SEGUROS BOLIVAR', 'HDI SEGUROS S.A', 'COMPAÑÍA MUNDIAL DE SEGUROS S A', 'Confianza', 
             'SBS SEGUROS COLOMBIA S.A.', 'SEGUROS DEL ESTADO S A', 'ASEGURADORA SOLIDARIA DE COLOMBIA', 'ALLIANZ SEGUROS S.A.', 
             'CHUBB DE COLOMBIA COMPAÑÍA SEGUROS S A', 'COMPAÑÍA ASEGURADORA DE FIANZAS S A'
         ]
-        
+
         self.cleaned_df['Otras'] = self.cleaned_df.apply(lambda row: row['SumaDePrima_Participacion'] if row['Aseguradora'] not in insurance_companies else 0, axis=1)
-        
+
         for company in insurance_companies:
             self.cleaned_df[company] = self.cleaned_df.apply(lambda row: row['SumaDePrima_Participacion'] if row['Aseguradora'] == company else 0, axis=1)
-    
+
     def group_data(self):
         self.grouped_df = self.cleaned_df.groupby('Ramo').agg(
             Primas_Totales=pd.NamedAgg(column='SumaDePrima_Participacion', aggfunc='sum'),
@@ -41,28 +40,14 @@ class ProduccionRamos:
             Finanzas=pd.NamedAgg(column='COMPAÑÍA ASEGURADORA DE FIANZAS S A', aggfunc='sum'),
             Otras=pd.NamedAgg(column='Otras', aggfunc='sum')
         ).reset_index()
-    
+
     def save_to_bytes(self):
         output = BytesIO()
         self.grouped_df.to_excel(output, index=False)
         output.seek(0)
         return output
-    
+
     def process_data(self):
         self.load_and_clean_data()
         self.add_insurance_columns()
         self.group_data()
-
-# Example usage:
-# file_path = 'BaseDatosJyA.xlsx'
-# produccion_ramos = ProduccionRamos(file_path)
-# produccion_ramos.process_data()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ProduccionRamos.py <file_path>")
-        sys.exit(1)
-    
-    file_path = sys.argv[1]
-    produccion_ramos = ProduccionRamos(file_path)
-    produccion_ramos.process_data()
